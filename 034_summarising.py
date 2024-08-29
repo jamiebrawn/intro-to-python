83916caf73bf609c4080223550c6d62fd194cd5e
Jamie Brawn <jameshenrybrawn@gmail.com>
2024-08-29 10:08:19 +0100

    completes 034_summarising.py

--- 034_summarising.py
+++ 034_summarising.py
@@ -54,7 +54,11 @@ print("Function: add_up_numbers")
 
 # Add up all the numbers in the list
 def add_up_numbers(numbers):
-  pass
+  list_sum = 0;
+  for number in numbers:
+    list_sum = list_sum + number
+  return list_sum
+    
 
 check_that_these_are_equal(
   add_up_numbers([1, 2, 3, 4]), 10)
